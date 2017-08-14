9858a3ccb82adba06868ba3f71527e08e5a95190
Heather Sosa <heatherlsosa@gmail.com>
2017-08-14 10:32:55 -0600

    App setup

---
+++ server.js
@@ -0,0 +1,13 @@
+//--- DEPENDANCIES --//
+var express = require("express");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+
+
+app.listen(PORT, function(){
+	console.log("Listening on port: " + PORT)
+});
